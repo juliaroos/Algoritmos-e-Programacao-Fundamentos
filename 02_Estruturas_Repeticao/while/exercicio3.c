4841c4ecd3dae9b805d8d8cb910975ca96882a47
Júlia Roos costa <ju.rooscosta@gmail.com>
2026-07-03 14:59:51 -0300

    Create exercicio3.c

---
+++ 02_Estruturas_Repeticao/while/exercicio3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int cont = 0; 
+    while (cont <= 2000){
+        int valor;
+        printf("%d \n", cont);
+        cont += 2;
+    }
+
+    return 0;
+}
