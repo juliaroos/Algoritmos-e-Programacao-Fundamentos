781503f60d6dfce20e48b1232fe21ab4c51c87c8
Júlia Roos costa <ju.rooscosta@gmail.com>
2026-07-06 15:13:24 -0300

    Create exercicio6.c

---
+++ 02_Estruturas_Repeticao/for/exercicio6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int cont; 
+    for (cont = 0; cont < 50; cont++){
+        int valor;
+        printf("Digite um valor: ");
+        scanf ("%d", &valor);
+    }
+
+    return 0;
+}
